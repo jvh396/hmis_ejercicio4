3a8a2273b6e150fc458f6a2d7f9db916c2e8396e
JUAN DE DIOS VALVERDE <jvh396@inlumine.ual.es>
2023-06-22 16:20:53 +0200

    Pagina personal Ejercicio 4

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HMIS PRACTICA 4</title>
+    <link rel="stylesheet" href="./pepe.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com"> 
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="contenedor">
+        <div class="seccion uno">
+            <div class="titulo">
+                <div>Ejercico 4</div>
+                <div>Herrramientas y métodos de Ingeniería del Software</div>
+            </div>
+        </div>
+        <div class="seccion dos">
+            <div class="contenedorSeccion1">
+                <div class="seccion1Titulo">
+                    Información personal
+                </div>
+                    <div class="seccion1izq">
+                        <ol>Cerveza</ol>
+						<ol>Padel</ol>
+						<ol>Salir con los amigos</ol>
+						<ol>Música</ol>
+                    </div>
+                    <div class="seccion1der">
+                        <ol><b>Enlaces de Interés</b></ol>
+						<ol> <a href="https://github.com/jvh396/hmis_ejercicio4" > Git Desarrollador</a></ol>
+						<ol> <a href="https://docs.github.com/es/get-started/quickstart/contributing-to-projects"> Forkear un proyecto en Github </a> </ol>
+						
+                    </div>
+            </div>
+
+        </div>
+        <div class="seccion tres">
+			<div class="contenedor3"> 
+				<div class="contenedor3izq">
+					 <div> Y aquí una foto personal </div> 
+				
+					
+				</div>			
+				<div class="contenedor3der"> 				<img src="./image/FOTO_PERFIL2.jpg">				</div>
+			</div>			
+		</div>
+       
+	</div>
+</body>
+
+</html>
